3d504290f321baf0ad0f24748f2c8da240adb62e
Intengine <studio@intengine.pl>
2018-07-25 08:56:21 +0200

    Set movie models
    Set up the model of movie, add ID, title, genre, release date and price.

---
+++ IntengineMovie/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IntengineMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string title { get; set; }
+        public DateTime releaseDate { get; set; }
+        public string genre { get; set; }
+        public decimal price { get; set; }
+    }
+}
