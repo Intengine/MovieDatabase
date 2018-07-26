b2f75721b8c76e188f109ebf48c7f1d837228fe3
Intengine <studio@intengine.pl>
2018-07-26 14:19:08 +0200

    Minor code changes
    Minor refactoring.

--- IntengineMovie/Migrations/IntengineMovieContextModelSnapshot.cs
+++ IntengineMovie/Migrations/IntengineMovieContextModelSnapshot.cs
@@ -3,9 +3,6 @@ using IntengineMovie.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
-using Microsoft.EntityFrameworkCore.Storage;
-using Microsoft.EntityFrameworkCore.Storage.Internal;
 using System;
 
 namespace IntengineMovie.Migrations
