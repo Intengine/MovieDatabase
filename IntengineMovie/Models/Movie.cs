0dc9f4827d4663585a9b43cf5809d332eb4e7428
Intengine <studio@intengine.pl>
2018-07-26 14:08:36 +0200

    Add "required", test on Azure
    Added "required" fields, tested website on Azure.

--- IntengineMovie/Models/Movie.cs
+++ IntengineMovie/Models/Movie.cs
@@ -8,6 +8,8 @@ namespace IntengineMovie.Models
     {
         public int ID { get; set; }
 
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         [Display(Name = "Title")]
         public string title { get; set; }
 
@@ -15,13 +17,21 @@ namespace IntengineMovie.Models
         [DataType(DataType.Date)]
         public DateTime releaseDate { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [StringLength(30)]
+        [Required]
         [Display(Name = "Genre")]
         public string genre { get; set; }
 
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Display(Name = "Price")]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal price { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string rating { get; set; }
     }
 }
