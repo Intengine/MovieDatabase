4084e8598bc61afbb0979d779693e8e913731151
Intengine <studio@intengine.pl>
2018-07-26 09:02:02 +0200

    Change display name
    Changed display name of title, release date, genre and price.

--- IntengineMovie/Models/Movie.cs
+++ IntengineMovie/Models/Movie.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IntengineMovie.Models
 {
     public class Movie
     {
         public int ID { get; set; }
+
+        [Display(Name = "Title")]
         public string title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime releaseDate { get; set; }
+
+        [Display(Name = "Genre")]
         public string genre { get; set; }
+
+        [Display(Name = "Price")]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal price { get; set; }
     }
 }
