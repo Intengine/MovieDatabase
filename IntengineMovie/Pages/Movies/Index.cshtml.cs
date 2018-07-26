77c01f2356fd980076ccabf63975ccf2bb6177d5
Intengine <studio@intengine.pl>
2018-07-26 09:50:42 +0200

    Add search bar
    Added search bar by title and list searching by genre.

--- IntengineMovie/Pages/Movies/Index.cshtml.cs
+++ IntengineMovie/Pages/Movies/Index.cshtml.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using IntengineMovie.Models;
 
@@ -15,11 +17,31 @@ namespace IntengineMovie.Movies
             _context = context;
         }
 
-        public IList<Movie> Movie { get;set; }
+        public IList<Movie> Movie { get; set; }
+        public SelectList Genres { get; set; }
+        public string MovieGenre { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string movieGenre, string searchString)
         {
-            Movie = await _context.Movie.ToListAsync();
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.genre
+                                            select m.genre;
+
+            var movies = from m in _context.Movie
+                         select m;
+
+            if(!string.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.title.Contains(searchString));
+            }
+
+            if(!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.genre == movieGenre);
+            }
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
