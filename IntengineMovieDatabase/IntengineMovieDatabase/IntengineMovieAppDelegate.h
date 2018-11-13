1fa1a5b5fea928ba654bf52a2452834fe9d1d084
Intengine <studio@intengine.pl>
2018-11-13 11:58:17 +0100

    Rebuild the project, added iCarousel library
    As in the title.

---
+++ IntengineMovieDatabase/IntengineMovieDatabase/IntengineMovieAppDelegate.h
@@ -0,0 +1,8 @@
+#import <Cocoa/Cocoa.h>
+
+@interface iCarouselMacAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (nonatomic, strong) IBOutlet NSWindow *window;
+@property (nonatomic, strong) IBOutlet NSWindowController *windowController;
+
+@end
---
+++ IntengineMovieDatabase/IntengineMovieDatabase/IntengineMovieAppDelegate.m
@@ -0,0 +1,13 @@
+#import "IntengineMovieAppDelegate.h"
+
+@implementation iCarouselMacAppDelegate
+
+@synthesize window;
+@synthesize windowController;
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(__unused NSApplication *)sender
+{
+    return YES;
+}
+
+@end
