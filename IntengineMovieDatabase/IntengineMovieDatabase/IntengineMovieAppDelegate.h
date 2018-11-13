2d981d153ddac023d8c9a768eed256851b61e9fc
Intengine <studio@intengine.pl>
2018-11-13 16:59:19 +0100

    Rebuilt project for React
    Another rebuilding, now for the web.

--- IntengineMovieDatabase/IntengineMovieDatabase/IntengineMovieAppDelegate.h
+++
@@ -1,8 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-@interface iCarouselMacAppDelegate : NSObject <NSApplicationDelegate>
-
-@property (nonatomic, strong) IBOutlet NSWindow *window;
-@property (nonatomic, strong) IBOutlet NSWindowController *windowController;
-
-@end
