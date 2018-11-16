80a2394cbd6570786dc4dc3bd633a1d1bb5e31ff
Intengine <studio@intengine.pl>
2018-11-16 15:21:06 +0100

    Added link to ModieDB
    Added button with link.

--- IntengineMovieDatabase/movie_database/src/MovieRow.js
+++ IntengineMovieDatabase/movie_database/src/MovieRow.js
@@ -1,13 +1,19 @@
 import React from 'react'
 
 class MovieRow extends React.Component {
+    viewMovie() {
+        const url = "https://themoviedb.org/movie/" + this.props.movie.id;
+        window.location.href = url;
+    }
+
     render() {
         return <table key={this.props.movie.id}>
             <tbody>
             <tr>
                 <td><img alt="movie poster" width="130" src={this.props.movie.poster_src}/></td>
-                <td>{this.props.movie.title}
+                <td><h3>{this.props.movie.title}</h3>
                     <p>{this.props.movie.overview}</p>
+                    <input type="button" onClick={this.viewMovie.bind(this)} value="View"/>
                 </td>
             </tr>
             </tbody>
