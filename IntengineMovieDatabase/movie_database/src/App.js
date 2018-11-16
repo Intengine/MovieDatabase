b87149927aa8003cc2124e96a3c61479f8889bc7
Intengine <studio@intengine.pl>
2018-11-16 14:46:41 +0100

    Got the API key, tried in the app
    As in description.

--- IntengineMovieDatabase/movie_database/src/App.js
+++ IntengineMovieDatabase/movie_database/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import MovieRow from './MovieRow.js';
+import $ from 'jquery';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    console.log("This is my initializer");
+    this.state = {};
+    /*console.log("This is my initializer");*/
 
-    const movies = [
+    /*const movies = [
         {id: 0, poster_src:"https://image.tmdb.org/t/p/w185/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg", title: "Avengers: Infinity War", overview: "Lorem ipsum dolor sit amet."},
         {id: 1, poster_src:"https://image.tmdb.org/t/p/w185/cezWGskPY5x7GaglTTRN4Fugfb8.jpg", title: "The Avengers", overview: "Lorem ipsum dolor sit amet."}
     ]
@@ -19,7 +21,28 @@ class App extends Component {
         movieRows.push(movieRow);
     });
 
-    this.state = {rows: movieRows};
+    this.state = {rows: movieRows};*/
+
+    this.performSearch();
+  }
+
+  performSearch() {
+      console.log("Perform search using moviedb API");
+      const urlString = "https://api.themoviedb.org/3/search/movie?query=marvel&api_key=1b5adf76a72a13bad99b8fc0c68cb085";
+      $.ajax({
+          url: urlString,
+          success: (searchResults) => {
+              console.log("Fetched data successfully");
+              const results = searchResults.results;
+
+              results.forEach((movie) => {
+                  console.log(movie.title);
+              });
+          },
+          error: (xhr, status, err) => {
+              console.error("Failed to fetch data");
+          }
+      });
   }
 
   render() {
